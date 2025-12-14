bcf9c6a6cf352ae79ab2a27fce7ed5b308736f24
onelngoma-source <onelngoma@gmail.com>
2025-12-14 20:41:17 +0100

    VERSION FINALE

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World from CI/CD Pipeline!');
+  res.status(200).send('Hello World - VERSION FINALE !');
 });
 
 module.exports = app;
